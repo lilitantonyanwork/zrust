86a098168c739cc03c3c7cf37ac19621c1f85f13
lilitantonyan <lilit@helix.am>
2025-03-24 00:00:19 +0400

    add landing page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.rating-list', {
     // Optional parameters
     slidesPerView: 3,
     spaceBetween: 20,
@@ -24,8 +24,48 @@ const swiper = new Swiper('.swiper', {
 
     },
 });
+const swiper1 = new Swiper('.daily-roulette-list', {
+    // Optional parameters
+    slidesPerView: 7,
+    spaceBetween: 20,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        700:{
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 7,
+            spaceBetween: 20,
+        },
+
+
+    },
+});
 $(function (){
     $('.mob-menu').on('click',function (){
         $('.menu').toggle();
     })
+    $('.btn-arrow').on('click',function (){
+        $('.gide-info-text').toggle();
+        $('.btn-arrow').toggleClass('btn-arrow-close');
+
+
+    });
+
+    $( ".landing-banner-item:nth-child(3)" )
+        .on( "mouseenter", function() {
+            $(".landing-banner-item" ).addClass('hover');
+        } )
+        .on( "mouseleave", function() {
+            $(".landing-banner-item" ).removeClass('hover');
+        } );
 })
